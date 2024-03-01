e6dd41201bc98aa2875dd0fb2ed171bcede20168
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-03-01 21:37:39 +0300

    ui.aceternity

---
+++ frontend/src/components/ui/backgound-gradient.tsx
@@ -0,0 +1,111 @@
+import { cn } from "./lib/utils";
+import React from "react";
+import { motion } from "framer-motion";
+
+export const BackgroundGradient = ({
+  children,
+  className,
+  containerClassName,
+  animate = true,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
+  animate?: boolean;
+}) => {
+  const variants = {
+    initial: {
+      backgroundPosition: "0 50%",
+    },
+    animate: {
+      backgroundPosition: ["0, 50%", "100% 50%", "0 50%"],
+    },
+  };
+  return (
+    <div className={cn("relative p-[4px] group", containerClassName)}>
+      <motion.div
+        variants={animate ? variants : undefined}
+        initial={animate ? "initial" : undefined}
+        animate={animate ? "animate" : undefined}
+        transition={
+          animate
+            ? {
+                duration: 5,
+                repeat: Infinity,
+                repeatType: "reverse",
+              }
+            : undefined
+        }
+        style={{
+          backgroundSize: animate ? "400% 400%" : undefined,
+        }}
+        className={cn(
+          "absolute inset-0 rounded-3xl z-[1] opacity-60 group-hover:opacity-100 blur-xl  transition duration-500",
+          " bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
+        )}
+      />
+      <motion.div
+        variants={animate ? variants : undefined}
+        initial={animate ? "initial" : undefined}
+        animate={animate ? "animate" : undefined}
+        transition={
+          animate
+            ? {
+                duration: 5,
+                repeat: Infinity,
+                repeatType: "reverse",
+              }
+            : undefined
+        }
+        style={{
+          backgroundSize: animate ? "400% 400%" : undefined,
+        }}
+        className={cn(
+          "absolute inset-0 rounded-3xl z-[1]",
+          "bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]"
+        )}
+      />
+
+      <div className={cn("relative z-10", className)}>{children}</div>
+    </div>
+  );
+};
+/**
+ * "use client";
+import React from "react";
+import { BackgroundGradient } from "../ui/background-gradient";
+import { IconAppWindow } from "@tabler/icons-react";
+import Image from "next/image";
+
+export function BackgroundGradientDemo() {
+  return (
+    <div>
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`/jordans.webp`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="mt-4 mb-2 text-base text-black sm:text-xl dark:text-neutral-200">
+          Air Jordan 4 Retro Reimagined
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+          February 17, 2024. Your best opportunity to get these right now is by
+          entering raffles and waiting for the official releases.
+        </p>
+        <button className="flex items-center py-1 pl-4 pr-1 mt-4 space-x-1 text-xs font-bold text-white bg-black rounded-full dark:bg-zinc-800">
+          <span>Buy now </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            $100
+          </span>
+        </button>
+      </BackgroundGradient>
+    </div>
+  );
+}
+
+ */
