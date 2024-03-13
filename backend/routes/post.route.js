f0f7e2d69db2d64258108af324961885aa11556a
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-03-13 20:17:54 +0300

    Refactoring

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -12,26 +12,8 @@ router.post(
   uploadPochette.upload
 );
 router.put("/update-post/:id", postController.updatePostController);
-router.delete("/delete-post/:id", postController.deletePostController);
 router.put("/like-post/:id", postController.likeAndDislikeController);
+router.delete("/delete-post/:id", postController.deletePostController);
 router.patch("/comment-post/:id", postController.commentPostController);
 
 module.exports = router;
-
-// const multer = require("multer");
-// const uploadFile = multer({ dest: "uploads/" });
-
-// router.post("/read-file", uploadFile.single("file"), (req, res) => {
-//   const file = req.file;
-//   if (!file) {
-//     return res.status(400).send("aucun fichier");
-//   }
-
-//   const fs = require("fs");
-//   fs.readFile(file.path, "utf-8", (err, data) => {
-//     if (err) {
-//       return res.status(500).send("Erreur lors de lecture du fichier");
-//     }
-//     console.log(data);
-//   });
-// });
