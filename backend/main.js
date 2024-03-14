19cd1f812248646dec56944a83c29521acf61530
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-03-14 13:00:06 +0300

    deployment test

--- backend/main.js
+++ backend/main.js
@@ -20,6 +20,9 @@ app.use(express.static("uploads"));
 
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 //jwt
 app.get("*", checkUser);
 app.get("/jwtId", requireAuth, (req, res) => {
